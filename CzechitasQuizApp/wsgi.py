92be769d02b9352eb6889bdd32d47db211ce10e5
pesikj <jiri.pesik@outlook.cz>
2023-12-27 23:38:12 +0100

    Azure settings

--- CzechitasQuizApp/wsgi.py
+++ CzechitasQuizApp/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'azureproject.production' if 'WEBSITE_HOSTNAME' in os.environ else 'azureproject.settings'
+settings_module = 'CzechitasQuizApp.production' if 'WEBSITE_HOSTNAME' in os.environ else 'CzechitasQuizApp.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
